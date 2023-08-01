59dc4f744186cc49be1491e4125f14db937d0a71
Vivek-Tiwari <127580268+VivekTiwari2003@users.noreply.github.com>
2023-08-01 16:23:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,76 @@
 //your code here
-document.getElementById("getUser").addEventListener("click", () => {
-    fetch("https://randomuser.me/api/")
-        .then((response) => response.json())
-        .then((data) => {
-            const user = data.results[0];
-            const name = `${user.name.first} ${user.name.last}`;
-            const photo = user.picture.large;
-            const age = user.dob.age;
-            const email = user.email;
-            const phone = user.phone;
 
-            document.querySelector(".user-photo").src = photo;
-            document.querySelector(".user-name").textContent = name;
-            document.querySelector(".info-age").textContent = `Age: ${age}`;
-            document.querySelector(".info-email").textContent = `Email: ${email}`;
-            document.querySelector(".info-phone").textContent = `Phone: ${phone}`;
+const userDetails = document.getElementById('userDetails');
+const userPhoto = document.getElementById('userPhoto');
+const userName = document.getElementById('userName');
+const additionalInfo = document.getElementById('additionalInfo');
+const ageInfo = document.getElementById('age');
+const emailInfo = document.getElementById('email');
+const phoneInfo = document.getElementById('phone');
+const getUserButton = document.getElementById('getUser');
+const infoButtons = document.querySelectorAll('[data-attr]');
 
-            // document.querySelector(".additional-info").style.display = "none";
-        })
-        .catch((error) => console.error("Error fetching random user:", error));
+// Function to fetch a random user from the API
+async function fetchRandomUser() {
+  const response = await fetch('https://randomuser.me/api/');
+  const data = await response.json();
+  return data.results[0];
+}
+
+// Function to display user details (name and photo)
+function displayUser(user) {
+  userPhoto.src = user.picture.large;
+  userName.textContent = `${user.name.first} ${user.name.last}`;
+}
+
+// Function to display additional user info (age, email, phone)
+function displayAdditionalInfo(user, infoType) {
+  switch (infoType) {
+    case 'age':
+      ageInfo.textContent = `Age: ${user.dob.age}`;
+      emailInfo.textContent = ''; // Clear other info
+      phoneInfo.textContent = ''; // Clear other info
+      break;
+    case 'email':
+      ageInfo.textContent = ''; // Clear other info
+      emailInfo.textContent = `Email: ${user.email}`;
+      phoneInfo.textContent = ''; // Clear other info
+      break;
+    case 'phone':
+      ageInfo.textContent = ''; // Clear other info
+      emailInfo.textContent = ''; // Clear other info
+      phoneInfo.textContent = `Phone: ${user.phone}`;
+      break;
+  }
+}
+
+// Function to clear additional user info
+function clearAdditionalInfo() {
+  ageInfo.textContent = '';
+  emailInfo.textContent = '';
+  phoneInfo.textContent = '';
+}
+
+// Event listener for info buttons
+infoButtons.forEach(button => {
+  button.addEventListener('click', async function() {
+    const user = await fetchRandomUser();
+    const infoType = button.getAttribute('data-attr');
+    displayAdditionalInfo(user, infoType);
+  });
 });
 
-document.querySelectorAll("button[data-attr]").forEach((button) => {
-    button.addEventListener("click", () => {
-        const dataType = button.getAttribute("data-attr");
-        const infoElement = document.querySelector(`.info-${dataType}`);
-
-        // Hide all additional info sections
-        document.querySelectorAll(".additional-info p").forEach((element) => {
-            if (element !== infoElement) {
-                element.style.display = "none";
-            }
-        });
-
-        // Display the selected additional info
-        if (infoElement) {
-            infoElement.style.display = "block";
-        }
-    });
+// Event listener for get new user button
+getUserButton.addEventListener('click', async function() {
+  const user = await fetchRandomUser();
+  displayUser(user);
+  clearAdditionalInfo();
 });
+
+// Initially hide additional user info
+
+// Get the first user and display details
+(async function() {
+  const user = await fetchRandomUser();
+  displayUser(user);
+})();
